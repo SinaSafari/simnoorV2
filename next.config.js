a88f0696649c6fa285a00e69305b32ce3b12349a
SinaSafari <m.sinasafari@gmail.com>
2020-04-26 11:20:20 +0430

    image opitimization

--- next.config.js
+++ next.config.js
@@ -1,4 +1,28 @@
+const withImages = require("next-optimized-images");
+const withPlugins = require("next-compose-plugins");
 const withCSS = require("@zeit/next-css");
-module.exports = withCSS({
-  /* config options here */
-});
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+
+// const nextConfig = {
+//   // exportTrailingSlash: true,
+//   webpack(config, options) {
+//     config.optimization.minimizer.push(new OptimizeCSSAssetsPlugin({}));
+
+//     return config;
+//   },
+// };
+
+module.exports = withPlugins(
+  [
+    [withCSS],
+    [
+      withImages,
+      {
+        optimizeImagesInDev: true,
+        handleImages: ["jpeg", "png", "svg"],
+        inlineImageLimit: -1,
+      },
+    ],
+  ]
+  // nextConfig
+);
