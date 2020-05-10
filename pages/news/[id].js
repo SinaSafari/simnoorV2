a278c3b77cd1b29b5aa2e383397f24f6a617a782
SinaSafari <m.sinasafari@gmail.com>
2020-05-10 22:51:36 +0430

    persian news

--- pages/news/[id].js
+++ pages/news/[id].js
@@ -1,14 +1,30 @@
-import { useRouter } from "next/router";
+import { useContext } from "react";
+import { AppContext } from "../../components/shared/context/AppContext";
 
 export default function SingleNews({ singleNews }) {
-  return (
-    <div className="container my-5">
-      <div class="jumbotron">
-        <h1 class="display-4 ">{singleNews[0].title}</h1>
+  const { isEng } = useContext(AppContext);
+
+  if (isEng) {
+    return (
+      <div className="container my-5">
+        <div class="jumbotron">
+          <h1 class="display-4 ">{singleNews[0].title}</h1>
+        </div>
+        <p>{singleNews[0].text}</p>
       </div>
-      <p>{singleNews[0].text}</p>
-    </div>
-  );
+    );
+  }
+
+  if (!isEng) {
+    return (
+      <div className="container my-5 rtlfont" style={{ textAlign: "right" }}>
+        <div class="jumbotron">
+          <h1 class="display-4 ">{singleNews[0].FATitle}</h1>
+        </div>
+        <p>{singleNews[0].FAText}</p>
+      </div>
+    );
+  }
 }
 
 SingleNews.getInitialProps = (ctx) => {
@@ -17,20 +33,31 @@ SingleNews.getInitialProps = (ctx) => {
     {
       id: 1,
       title: "News Title No.01",
+      FATitle: "عنوان شماره ۱",
       text:
         "No.01 Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of de Finibus Bonorum et Malorum (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, Lorem ipsum dolor sit amet.., comes from a line in section 1.10.32.The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from de Finibus Bonorum et Malorum by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.",
+      FAText:
+        "بندی برای پر کردن صفحه و ارایه اولیه شکل ظاهری و کلی طرح سفارش گرفته شده استفاده می نماید، تا از نظر گرافیکی نشانگر چگونگی نوع و اندازه فونت و ظاهر متن باشد. معمولا طراحان گرافیک برای صفحه‌آرایی، نخست از متن‌های آزمایشی و بی‌معنی استفاده می‌کنند تا صرفا به مشتری یا صاحب کار خود نشان دهند که صفحه طراحی یا صفحه بندی شده بعد از اینکه متن در آن قرار گیرد چگونه به نظر می‌رسد و قلم‌ها و اندازه‌بندی‌ها چگونه در نظر گرفته شده‌است. از آنجایی که طراحان عموما نویسنده متن نیستند و وظیفه رعایت حق تکثیر متون را ندارند و در همان حال کار آنها به نوعی وابسته به متن می‌باشد آنها با استفاده از محتویات ساختگی، صفحه گرافیکی خود را صفحه‌آرایی می‌کنند تا مرحله طراحی و صفحه‌بندی را به پایان برند. بندی برای پر کردن صفحه و ارایه اولیه شکل ظاهری و کلی طرح سفارش گرفته شده استفاده می نماید، تا از نظر گرافیکی نشانگر چگونگی نوع و اندازه فونت و ظاهر متن باشد. معمولا طراحان گرافیک برای صفحه‌آرایی، نخست از متن‌های آزمایشی و بی‌معنی استفاده می‌کنند تا صرفا به مشتری یا صاحب کار خود نشان دهند که صفحه طراحی یا صفحه بندی شده بعد از اینکه متن در آن قرار گیرد چگونه به نظر می‌رسد و قلم‌ها و اندازه‌بندی‌ها چگونه در نظر گرفته شده‌است. از آنجایی که طراحان عموما نویسنده متن نیستند و وظیفه رعایت حق تکثیر متون را ندارند و در همان حال کار آنها به نوعی وابسته به متن می‌باشد آنها با استفاده از محتویات ساختگی، صفحه گرافیکی خود را صفحه‌آرایی می‌کنند تا مرحله طراحی و صفحه‌بندی را به پایان برند.",
     },
     {
       id: 2,
       title: "News Title No.02",
+      FATitle: "عنوان شماره ۲",
+
       text:
         "No.02 Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.",
+      FAText:
+        "بندی برای پر کردن صفحه و ارایه اولیه شکل ظاهری و کلی طرح سفارش گرفته شده استفاده می نماید، تا از نظر گرافیکی نشانگر چگونگی نوع و اندازه فونت و ظاهر متن باشد. معمولا طراحان گرافیک برای صفحه‌آرایی، نخست از متن‌های آزمایشی و بی‌معنی استفاده می‌کنند تا صرفا به مشتری یا صاحب کار خود نشان دهند که صفحه طراحی یا صفحه بندی شده بعد از اینکه متن در آن قرار گیرد چگونه به نظر می‌رسد و قلم‌ها و اندازه‌بندی‌ها چگونه در نظر گرفته شده‌است. از آنجایی که طراحان عموما نویسنده متن نیستند و وظیفه رعایت حق تکثیر متون را ندارند و در همان حال کار آنها به نوعی وابسته به متن می‌باشد آنها با استفاده از محتویات ساختگی، صفحه گرافیکی خود را صفحه‌آرایی می‌کنند تا مرحله طراحی و صفحه‌بندی را به پایان برند. بندی برای پر کردن صفحه و ارایه اولیه شکل ظاهری و کلی طرح سفارش گرفته شده استفاده می نماید، تا از نظر گرافیکی نشانگر چگونگی نوع و اندازه فونت و ظاهر متن باشد. معمولا طراحان گرافیک برای صفحه‌آرایی، نخست از متن‌های آزمایشی و بی‌معنی استفاده می‌کنند تا صرفا به مشتری یا صاحب کار خود نشان دهند که صفحه طراحی یا صفحه بندی شده بعد از اینکه متن در آن قرار گیرد چگونه به نظر می‌رسد و قلم‌ها و اندازه‌بندی‌ها چگونه در نظر گرفته شده‌است. از آنجایی که طراحان عموما نویسنده متن نیستند و وظیفه رعایت حق تکثیر متون را ندارند و در همان حال کار آنها به نوعی وابسته به متن می‌باشد آنها با استفاده از محتویات ساختگی، صفحه گرافیکی خود را صفحه‌آرایی می‌کنند تا مرحله طراحی و صفحه‌بندی را به پایان برند. بندی برای پر کردن صفحه و ارایه اولیه شکل ظاهری و کلی طرح سفارش گرفته شده استفاده می نماید، تا از نظر گرافیکی نشانگر چگونگی نوع و اندازه فونت و ظاهر متن باشد. معمولا طراحان گرافیک برای صفحه‌آرایی، نخست از متن‌های آزمایشی و بی‌معنی استفاده می‌کنند تا صرفا به مشتری یا صاحب کار خود نشان دهند که صفحه طراحی یا صفحه بندی شده بعد از اینکه متن در آن قرار گیرد چگونه به نظر می‌رسد و قلم‌ها و اندازه‌بندی‌ها چگونه در نظر گرفته شده‌است. از آنجایی که طراحان عموما نویسنده متن نیستند و وظیفه رعایت حق تکثیر متون را ندارند و در همان حال کار آنها به نوعی وابسته به متن می‌باشد آنها با استفاده از محتویات ساختگی، صفحه گرافیکی خود را صفحه‌آرایی می‌کنند تا مرحله طراحی و صفحه‌بندی را به پایان برند.",
     },
     {
       id: 3,
       title: "News Title No.03",
+      FATitle: "عنوان شماره ۳",
+
       text:
         "No.03 Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).",
+      FAText:
+        "بندی برای پر کردن صفحه و ارایه اولیه شکل ظاهری و کلی طرح سفارش گرفته شده استفاده می نماید، تا از نظر گرافیکی نشانگر چگونگی نوع و اندازه فونت و ظاهر متن باشد. معمولا طراحان گرافیک برای صفحه‌آرایی، نخست از متن‌های آزمایشی و بی‌معنی استفاده می‌کنند تا صرفا به مشتری یا صاحب کار خود نشان دهند که صفحه طراحی یا صفحه بندی شده بعد از اینکه متن در آن قرار گیرد چگونه به نظر می‌رسد و قلم‌ها و اندازه‌بندی‌ها چگونه در نظر گرفته شده‌است. از آنجایی که طراحان عموما نویسنده متن نیستند و وظیفه رعایت حق تکثیر متون را ندارند و در همان حال کار آنها به نوعی وابسته به متن می‌باشد آنها با استفاده از محتویات ساختگی، صفحه گرافیکی خود را صفحه‌آرایی می‌کنند تا مرحله طراحی و صفحه‌بندی را به پایان برند. بندی برای پر کردن صفحه و ارایه اولیه شکل ظاهری و کلی طرح سفارش گرفته شده استفاده می نماید، تا از نظر گرافیکی نشانگر چگونگی نوع و اندازه فونت و ظاهر متن باشد. معمولا طراحان گرافیک برای صفحه‌آرایی، نخست از متن‌های آزمایشی و بی‌معنی استفاده می‌کنند تا صرفا به مشتری یا صاحب کار خود نشان دهند که صفحه طراحی یا صفحه بندی شده بعد از اینکه متن در آن قرار گیرد چگونه به نظر می‌رسد و قلم‌ها و اندازه‌بندی‌ها چگونه در نظر گرفته شده‌است. از آنجایی که طراحان عموما نویسنده متن نیستند و وظیفه رعایت حق تکثیر متون را ندارند و در همان حال کار آنها به نوعی وابسته به متن می‌باشد آنها با استفاده از محتویات ساختگی، صفحه گرافیکی خود را صفحه‌آرایی می‌کنند تا مرحله طراحی و صفحه‌بندی را به پایان برند. بندی برای پر کردن صفحه و ارایه اولیه شکل ظاهری و کلی طرح سفارش گرفته شده استفاده می نماید، تا از نظر گرافیکی نشانگر چگونگی نوع و اندازه فونت و ظاهر متن باشد. معمولا طراحان گرافیک برای صفحه‌آرایی، نخست از متن‌های آزمایشی و بی‌معنی استفاده می‌کنند تا صرفا به مشتری یا صاحب کار خود نشان دهند که صفحه طراحی یا صفحه بندی شده بعد از اینکه متن در آن قرار گیرد چگونه به نظر می‌رسد و قلم‌ها و اندازه‌بندی‌ها چگونه در نظر گرفته شده‌است. از آنجایی که طراحان عموما نویسنده متن نیستند و وظیفه رعایت حق تکثیر متون را ندارند و در همان حال کار آنها به نوعی وابسته به متن می‌باشد آنها با استفاده از محتویات ساختگی، صفحه گرافیکی خود را صفحه‌آرایی می‌کنند تا مرحله طراحی و صفحه‌بندی را به پایان برند.",
     },
   ];
 
--- pages/news/index.js
+++ pages/news/index.js
@@ -1,78 +1,110 @@
 import Link from "next/link";
+import { useContext } from "react";
+import { AppContext } from "../../components/shared/context/AppContext";
 
 const NewsList = [
   {
     id: 1,
     title: "News Title No.01",
+    FATitle: "عنوان شماره ۱",
     summeryText:
       "No.01 Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ",
+    FASummeryText:
+      "لورم ایپسوم یا طرح‌نما  به متنی آزمایشی و بی‌معنی در صنعت چاپ، صفحه‌آرایی و طراحی گرافیک گفته می‌شود. طراح گرافیک از ا",
   },
   {
     id: 2,
     title: "News Title No.02",
+    FATitle: "عنوان شماره ۲",
     summeryText:
       "No.02 Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when ",
+    FASummeryText:
+      "لورم ایپسوم یا طرح‌نما  به متنی آزمایشی و بی‌معنی در صنعت چاپ، صفحه‌آرایی و طراحی گرافیک گفته می‌شود. طراح گرافیک از ا",
   },
   {
     id: 3,
     title: "News Title No.03",
+    FATitle: "عنوان شماره ۳",
     summeryText:
       "No.03 Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took ",
+    FASummeryText:
+      "لورم ایپسوم یا طرح‌نما  به متنی آزمایشی و بی‌معنی در صنعت چاپ، صفحه‌آرایی و طراحی گرافیک گفته می‌شود. طراح گرافیک از ا",
   },
 ];
 export default function News() {
-  return (
-    <div className="container my-5">
-      <div className="row">
-        <div className="col-lg-3 col-md-3 col-sm-12 ">
-          <h4 className="mb-4">
-            <span style={{ borderBottom: "3px solid orange" }}>News List</span>
-          </h4>
-          <ul className="list-group list-group my-3">
+  const { isEng } = useContext(AppContext);
+
+  if (isEng) {
+    return (
+      <div className="container my-5">
+        <div className="row">
+          <div className="col-lg-12 col-md-12 col-sm-12 ">
+            <h4 className="mb-4">
+              <span style={{ borderBottom: "3px solid orange" }}>News</span>
+            </h4>
             {NewsList.map((e, i) => (
-              <li className="list-group-item">
-                <Link as={`/news/${e.id}`} href="/news/[id]">
-                  <a>{e.title}</a>
-                </Link>
-              </li>
-            ))}
-          </ul>
-        </div>
-        <div className="col-lg-9 col-md-9 col-sm-12 ">
-          <h4 className="mb-4">
-            <span style={{ borderBottom: "3px solid orange" }}>News</span>
-          </h4>
-          {NewsList.map((e, i) => (
-            // <div className="jumbotron" key={i}>
-            //   <h1 className="display-4">{e.title}</h1>
-            //   <p className="lead">{e.summeryText}</p>
-            //   <Link as={`/news/${e.id}`} href="/news/[id]">
-            //     <a className="btn btn-primary btn-lg" href="#" role="button">
-            //       Learn more
-            //     </a>
-            //   </Link>
-            // </div>
-            <div class="card mb-3" style={{ Width: "100%" }}>
-              <div class="row no-gutters">
-                <div class="col-md-4">
-                  <img src="/img/bg1.jpg" class="card-img" alt="..." />
-                </div>
-                <div class="col-md-8">
-                  <div class="card-body">
-                    <h5 class="card-title">{e.title}</h5>
-                    <p class="card-text">{e.summeryText}</p>
-                    <p class="card-text">
-                      <Link as={`/news/${e.id}`} href="/news/[id]">
-                        <a>Read More</a>
-                      </Link>
-                    </p>
+              <div
+                class="card mb-5"
+                style={{ Width: "100%", borderColor: "orange" }}
+              >
+                <div class="row no-gutters">
+                  <div class="col-md-4">
+                    <img src="/img/bg1.jpg" class="card-img" alt="..." />
+                  </div>
+                  <div class="col-md-8">
+                    <div class="card-body">
+                      <h5 class="card-title">{e.title}</h5>
+                      <p class="card-text">{e.summeryText}</p>
+                      <p class="card-text">
+                        <Link as={`/news/${e.id}`} href="/news/[id]">
+                          <a>Read More</a>
+                        </Link>
+                      </p>
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
-          ))}
+            ))}
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
+
+  if (!isEng) {
+    return (
+      <div className="container my-5 rtlfont" style={{ textAlign: "right" }}>
+        <div className="row">
+          <div className="col-lg-12 col-md-12 col-sm-12 ">
+            <h4 className="mb-4">
+              <span style={{ borderBottom: "3px solid orange" }}>اخبار</span>
+            </h4>
+            {NewsList.map((e, i) => (
+              <div
+                class="card mb-5"
+                style={{ Width: "100%", borderColor: "orange" }}
+              >
+                <div class="row no-gutters">
+                  <div class="col-md-8">
+                    <div class="card-body">
+                      <h5 class="card-title">{e.FATitle}</h5>
+                      <p class="card-text">{e.FASummeryText}</p>
+                      <p class="card-text">
+                        <Link as={`/news/${e.id}`} href="/news/[id]">
+                          <a>ادامه مطلب</a>
+                        </Link>
+                      </p>
+                    </div>
+                  </div>
+                  <div class="col-md-4">
+                    <img src="/img/bg1.jpg" class="card-img" alt="..." />
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
